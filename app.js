ef73e26a48399c4941494b40c7114ecf355fde95
karim1706 <47336595+karim1706@users.noreply.github.com>
2020-10-24 18:58:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -29,5 +29,39 @@ touch.forEach(element => {
       })
 
     })
+
+    // Gestion du tactile sur mobiles
+
+    element.addEventListener('touchstart', function touchStart(k) {
+            k.preventDefault();
+            let pianoKey = k.target; console.log(pianoKey);
+            let touch = k.touches[0]; console.log(touch);
+
+             // Récupérer le data-id
+        let id = element.dataset.id; console.log(id);
+        // Jouer une mélodie au click
+        let audio = document.querySelector(`audio[data-id=${id}]`);
+
+        if(!audio) return;
+        audio.play();
+
+          // Si on lance un autre audio, l'audio précédent s'arrête
+        document.addEventListener('play', function(e){
+                var audios = document.getElementsByTagName('audio');
+                for(var i = 0; i < audios.length; i++){
+                    if(audios[i] != e.target){
+                        audios[i].pause();
+                    }
+                }
+            }, true);
+
+            let touches = document.querySelector(`.touch[data-id=${id}]`);
+            touches.classList.add('scale');
+
+            touches.addEventListener('touchend', () => {
+                touches.classList.remove('scale');
+            })
+      
+    }, false);
     
 })
